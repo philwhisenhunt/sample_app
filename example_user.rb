32dae653f420d7787a37b84b4ad760fbb44e6f77
Phil Whisenhunt <philip.whisenhunt@gmail.com>
2020-04-16 16:49:45 -0400

    Update example_user.rb
    Built out first and last name functionality

--- example_user.rb
+++ example_user.rb
@@ -1,12 +1,18 @@
-  class User
-    attr_accessor :name, :email
+class User
+    attr_accessor :first_name, :last_name, :email
 
     def initialize(attributes = {})
-        @name = attributes[:name]
+        @first_name = attributes[:first_name]
+        @last_name = attributes[:last_name]
         @email = attributes[:email]
-  end
+    end
 
-  def formatted_email
-    "#{@name} <#{@email}>"
-  end
+    def formatted_email
+        "#{full_name} <#{@email}>"
+    end
+
+
+    def full_name
+        "#{@first_name} #{@last_name}"
+    end
 end
