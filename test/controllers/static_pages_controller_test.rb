3ffac91002c155f763c9cc6075c49a2d4fc8de2e
Phil Whisenhunt <philip.whisenhunt@gmail.com>
2020-04-20 08:32:21 -0400

    Confirm contact page is working

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -46,6 +46,12 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     get root_url
     assert_select 'a', text: 'Ruby on Rails Tutorial'
   end
-  
+
+  test "should get contact" do
+    get static_pages_contact_url
+    assert_response :success
+    assert_select "title", "Contact | Ruby on Rails Tutorial Sample App"
+    
+  end
 
 end
