d03254d7e1c7bcf6d1588aca55a6044588e7ba53
Phil Whisenhunt <philip.whisenhunt@gmail.com>
2020-04-30 15:07:33 -0400

    Now testing for errors

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -1,6 +1,24 @@
 require 'test_helper'
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
- 
 
+  test "invalid signup information" do
+    get signup_path
+    assert_no_difference 'User.count' do
+      post users_path, params: { user: { name:  "",
+                                         email: "user@invalid",
+                                         password:              "foo",
+                                         password_confirmation: "bar" } }
+    end
+    assert_template 'users/new'
+   
+    assert_select 'div#error_explanation'
+    assert_select 'div.alert'
+    assert_select 'div.field_with_errors'
+    
+  end
+
+  test "Errors are visible upon signup" do
+
+  end
 end
