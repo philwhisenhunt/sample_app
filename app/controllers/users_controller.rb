9e83d61908993c9658ef38925acbbf51d4d5062c
Phil Whisenhunt <philip.whisenhunt@gmail.com>
2020-05-05 19:40:33 -0400

    Update users_controller.rb
    Added a flash statement

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,6 +14,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       # Handle a successful save.
+      flash[:success] = "Welcome to the Sample App!"
     else
       render 'new'
     end
