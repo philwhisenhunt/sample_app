276c1285d48a4930cd2165432f2e53d3dd3e5d83
Phil Whisenhunt <philip.whisenhunt@gmail.com>
2020-05-28 08:52:05 -0400

    Example flash message

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,6 +8,7 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:session][:password])
             #Log the user in and redirect to the user's show page.
     else
+      flash[:danger] = 'Invalid email/password combination' # Not quite right yet
       render 'new'
     end
   end
