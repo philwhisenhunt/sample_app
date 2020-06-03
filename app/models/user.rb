af8d75a36cbb05dc9cf79a7bdce4156ee25b7e7b
Phil Whisenhunt <philip.whisenhunt@gmail.com>
2020-06-03 10:42:49 -0400

    Adjusted to get closer to example test from guide

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  before_save { email.downcase! }
+  before_save { self.email = email.downcase }
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
@@ -12,7 +12,7 @@ class User < ApplicationRecord
   def User.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
                                                   BCrypt::Engine.cost
-    BCrypt::Password.create(string cost: cost)
+    BCrypt::Password.create(string, cost: cost)
   end
 
 end
--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -24,6 +24,7 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert_template 'users/show'
     #This makes sure there are no extra login paths on the page
     assert_select "a[href=?]", login_path, count: 0
+    assert_select "a[href=?", logout_path
     assert_select "a[href=?]", user_path(@user)
   end
 end
