8408cbafb11850d6df36a9c3cdf9b627aed49fc7
Phil Whisenhunt <philip.whisenhunt@gmail.com>
2020-06-08 08:39:12 -0400

    Add missing class info

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,11 +9,16 @@ class User < ApplicationRecord
   has_secure_password
   validates :password, presence: true, length: { minimum: 5 }
 
+  class << self
   #Returns the hash digest of the given string.
   def User.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
                                                   BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
+  end
+
+  def new_token
+    SecureRandom.urlsafe_base64
   end
 
   def User.new_token
