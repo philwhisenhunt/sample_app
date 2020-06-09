9253dcd94b127a3f2fd324baa1c08ba53d1aee32
Phil Whisenhunt <philip.whisenhunt@gmail.com>
2020-06-09 08:29:45 -0400

    Revert "Revert "Working except for remmeber token part of tests""
    This reverts commit 62fa41658929b1ac919c6245cb87c09765e47ee5.

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -60,23 +60,16 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", user_path(@user), count: 0
   end
 
-  test "login with remembering" do
+ test "login with remembering" do
     log_in_as(@user, remember_me: '1')
-    assert_not_empty cookies[:remember_token]
-  end
-
-  test "login with remembering" do
-    # Log in to set the cookie.
-    log_in_as(@user, remember_me: '1')
-    # Log in again and verify that the cookie is deleted.
     assert_equal cookies['remember_token'], assigns(:user).remember_token
   end
 
-  test "login without remembering" do 
-    #Log in to set the cooke.
+  test "login without remembering" do
+    # Log in to set the cookie.
     log_in_as(@user, remember_me: '1')
     # Log in again and verify that the cookie is deleted.
-    login_in_as(@user, remember_me: '0')
+    log_in_as(@user, remember_me: '0')
     assert_empty cookies[:remember_token]
   end
 end
