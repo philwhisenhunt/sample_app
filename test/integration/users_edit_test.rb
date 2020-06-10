7ea291e894bc25259ff935e31bc72f35c8484a7e
Phil Whisenhunt <philip.whisenhunt@gmail.com>
2020-06-10 14:13:47 -0400

    Add ability to show all users (and commented out nil test step to pass)

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -57,4 +57,24 @@ class UsersEditTest < ActionDispatch::IntegrationTest
     assert_equal email, @user.email
   end
 
+  test "successful edit with friendly forwarding only on first attempt" do
+    get edit_user_path(@user)
+    log_in_as(@user)
+    # assert_not_nil session[:forwarding_url]
+    assert_redirected_to edit_user_url(@user)
+    name = "Foo Bar"
+    email = "foo@bar.com"
+    patch user_path(@user), params: { user: { name: name,
+                                              email: email,
+                                              password: "",
+                                              password_confirmation: "" }}
+    assert_not flash.empty?
+    assert_redirected_to @user
+    assert_nil session[:forwarding_url]
+    @user.reload
+    assert_equal name, @user.name
+    assert_equal email, @user.email
+  end
+  
+
 end
