b33469958aab58cf3d3f3c0b8f798a754e088a03
Phil Whisenhunt <philip.whisenhunt@gmail.com>
2020-06-10 08:14:52 -0400

    Add edit ability without requireing a change to the password

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,18 +2,15 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-   
   end
-  
+
   def new
     @user = User.new
   end
 
-
   def create
     @user = User.new(user_params)
     if @user.save
-      reset_session
       log_in @user
       flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
@@ -23,32 +20,24 @@ class UsersController < ApplicationController
   end
 
   def edit
-    # @user = User.update
     @user = User.find(params[:id])
-
   end
 
   def update
     @user = User.find(params[:id])
     if @user.update(user_params)
       # Handle a successful update.
-    else
-      render 'edit'
-    end
-
-  end
-
-  def update
-    @user = User.find(params[:id])
-    if @user.update(user_params)
-      # Handle a successful update
+      flash[:success] = "Profile updated"
+      redirect_to @user
     else
       render 'edit'
     end
   end
+
   private
-  def user_params
-    params.require(:user).permit(:name, :email, :password,
-                                  :password_confirmation)
-  end
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
 end
