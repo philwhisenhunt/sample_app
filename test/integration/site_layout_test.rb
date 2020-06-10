a5b8ef02e925c684a1620b7b61c53523476bd1a8
Phil Whisenhunt <philip.whisenhunt@gmail.com>
2020-06-10 14:25:48 -0400

    Fixed typo

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -15,7 +15,7 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     get root_path
     assert_select "a[href=?]", logout_path
     assert_select "a[href=?]", users_path
-    assert_select "a[href=?]", users_path(user)
+    assert_select "a[href=?]", user_path(user)
     assert_select "a[href=?]", edit_user_path(user)
   end
 
