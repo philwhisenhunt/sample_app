caba07eb94f7939c97b90cbbc07f0d644a05f71c
Phil Whisenhunt <philip.whisenhunt@gmail.com>
2020-06-10 11:17:38 -0400

    Remembers url and tests for it

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -44,14 +44,14 @@ class UsersController < ApplicationController
 
     #Before filters
 
-    #Confirms a logged in user
-  
-    def logged_in_user
-      unless logged_in?
-        flash[:danger] = "Please log in."
-        redirect_to login_url
-      end
+ # Confirms a logged-in user.
+  def logged_in_user
+    unless logged_in?
+      store_location
+      flash[:danger] = "Please log in."
+      redirect_to login_url
     end
+  end
 
     def correct_user
       @user = User.find(params[:id])
