6fb902bca2dfe7a4c5bdaedd43bff07f2fc21e40
Phil Whisenhunt <philip.whisenhunt@gmail.com>
2020-06-19 15:34:46 -0400

    Add test for valid account activation

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -2,6 +2,10 @@ require 'test_helper'
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
 
+  def setup
+    ActionMailer::Base.deliveries.clear
+  end
+
   test "invalid signup information" do
     get signup_path
     assert_no_difference 'User.count' do
@@ -17,6 +21,16 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     assert_select 'div.field_with_errors'
     
   end
+
+
+   test "valid signup information with account activation" do
+    get signup_path
+    assert_difference 'user.count', 1 do
+      post users_path, params: { user: { name: "Example User",
+                                          email: "user@example.com",
+                                          password: "password",
+                                          password_confirmation: "password" } }
+    end
 
   test "Errors are visible upon signup" do
     get signup_path
