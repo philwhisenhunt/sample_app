8c180a6aa8a49ce805df84de7a99decea5f689a0
Phil Whisenhunt <philip.whisenhunt@gmail.com>
2020-06-24 22:11:22 -0400

    Formatting adjustment

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,7 +20,7 @@ end
   def create
     @user = User.new(user_params)
     if @user.save
-      @user.send_activation_email
+      UserMailer.account_activation(@user).deliver_now
       flash[:info] = "Please check your email to activate your account."
       redirect_to root_url
     else
