1fed6239f8009d2206db56a9f197131aedbfbdf2
Phil Whisenhunt <philip.whisenhunt@gmail.com>
2020-07-01 14:40:03 -0400

    add password_reset_expired? method

--- app/models/user.rb
+++ app/models/user.rb
@@ -69,6 +69,16 @@ class User < ApplicationRecord
     UserMailer.password_reset(self).deliver_now
   end
 
+  # Returns true if a password reset has expired.
+  def password_reset_expired?
+    reset_sent_at < 2.hours.ago
+  ​end
+
+  # Returns true if a password reset has expired.
+  def password_reset_expired?
+    reset_sent_at < 2.hours.ago
+  end
+
   private 
   #Converts an email to all downcase
   def downcase_email
