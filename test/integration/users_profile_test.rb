b683985ff175addf7e3ee667af40c0e8ec6b6261
Phil Whisenhunt <philip.whisenhunt@gmail.com>
2020-07-07 10:29:02 -0400

    Spelling fix

--- test/integration/users_profile_test.rb
+++ test/integration/users_profile_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 class UsersProfileTest < ActionDispatch::IntegrationTest
- include ApplicationHelper
+  include ApplicationHelper
 
   def setup
     @user = users(:michael)
@@ -9,11 +9,11 @@ class UsersProfileTest < ActionDispatch::IntegrationTest
 
   test "profile display" do
     get user_path(@user)
-    assert_tempalte 'users/show'
+    assert_template 'users/show'
     assert_select 'title', full_title(@user.name) 
     assert_select 'h1', text: @user.name 
     assert_select 'h1>img.gravatar'
-    assert_match @user.microposts.count.to_s, reponse.body 
+    assert_match @user.microposts.count.to_s, response.body 
     assert_select 'div.pagination' 
     @user.microposts.paginate(page: 1).each do |micropost|
       assert_match micropost.content, response.body 
