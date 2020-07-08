0f323c6d521d8dc1353ca672daf815a5aa6b4656
Phil Whisenhunt <philip.whisenhunt@gmail.com>
2020-07-08 11:55:59 -0400

    Complete destroy method

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -17,6 +17,11 @@ class MicropostsController < ApplicationController
         @micropost.destroy
         flash[:success] = "Micropost deleted"
         # redirect to be determined
+        if request.referrer.nil? || request.referrer == microposts_url
+            redirect_to root_url
+        else
+            redirect_to request.referrer
+        end
     end
 
     private
