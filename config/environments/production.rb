8cdf311a4749c8e1d8ca6e2172a422be9fe3c1ca
Phil Whisenhunt <philip.whisenhunt@gmail.com>
2020-07-13 13:22:23 -0400

    Add production details

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,8 @@
 Rails.application.configure do
+
+# Store uploaded files on Amazon AWS.
+config.active_storage.service = :amazon
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
