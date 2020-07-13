14f78ababddcdfbaddc64f4acb48ccee24572054
Phil Whisenhunt <philip.whisenhunt@gmail.com>
2020-07-13 15:52:38 -0400

    Fix user login problem

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,6 +20,7 @@ end
   def create
     @user = User.new(user_params)
     if @user.save
+      log_in @user
       @user.send_activation_email
       flash[:info] = "Please check your email to activate your account."
       redirect_to root_url
