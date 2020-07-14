a1ea249a6bedcbb866d7bfa3ad2df0a74b8f0757
Phil Whisenhunt <philip.whisenhunt@gmail.com>
2020-07-14 10:05:10 -0400

    Checking in progress before adjusting to fix exercise error

--- db/migrate/20200714124859_create_relationships.rb
+++ db/migrate/20200714124859_create_relationships.rb
@@ -3,11 +3,12 @@ class CreateRelationships < ActiveRecord::Migration[6.0]
     create_table :relationships do |t|
       t.integer :follower_id
       t.integer :followed_id
-      add_index :relationships, :follower_id
-      add_index :relationships, :followed_id
-      add_index :relationships, [:follower_id, :followed_id], unique: true
-
+    
       t.timestamps
     end
+    add_index :relationships, :follower_id
+    add_index :relationships, :followed_id
+    add_index :relationships, [:follower_id, :followed_id], unique: true
+
   end
 end
