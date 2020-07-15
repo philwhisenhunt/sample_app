f8b5aad778d1c371529342efb4706c3b1a6dbb0d
Phil Whisenhunt <philip.whisenhunt@gmail.com>
2020-07-15 09:11:12 -0400

    Matching formatting

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -90,7 +90,7 @@ class UserTest < ActiveSupport::TestCase
 
   test "should follow and unfollow a user" do
     michael = users(:michael)
-    archer = users(:archer)
+    archer  = users(:archer)
     assert_not michael.following?(archer)
     michael.follow(archer)
     assert michael.following?(archer)
