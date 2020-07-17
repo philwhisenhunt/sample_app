1962dd03a2b96e4da50d7dfc3d58e1e1102cce3f
Phil Whisenhunt <philip.whisenhunt@gmail.com>
2020-07-17 12:14:21 -0400

    Fixed typos

--- app/models/user.rb
+++ app/models/user.rb
@@ -86,10 +86,10 @@ class User < ApplicationRecord
   end
 
   def feed
-    following_ids = "SELECT followed id FROM relationships
-    WHERE follower_id = :user_id"
+    following_ids = "SELECT followed_id FROM relationships
+                     WHERE  follower_id = :user_id"
     Micropost.where("user_id IN (#{following_ids})
-    OR user_id = :user_id", user_id: id)
+                     OR user_id = :user_id", user_id: id)
   end
 
   #Follows a user.
