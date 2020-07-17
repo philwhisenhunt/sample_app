f0cb74c6b8de4e27ada63fc991f01814c5d02616
Phil Whisenhunt <philip.whisenhunt@gmail.com>
2020-07-17 08:53:00 -0400

    Add to AJAX response

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -12,8 +12,12 @@ class RelationshipsController < ApplicationController
     end
 
     def destroy
-        user = Relationship.find(params[:id]).followed
-        current_user.unfollow(user)
+        @user = Relationship.find(params[:id]).followed
+        current_user.unfollow(@user)
+        respond_to do |format|
+            format.html {redirect_to @user}
+            format.js
+        end
         redirect_to user
     end
 end
